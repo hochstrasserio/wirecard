6d51b8f6c3cf65650812b0f16f300b3969b59be6
Christoph Hochstrasser <me@christophh.net>
2015-08-24 20:50:38 +0200

    Disable sending InitCheckoutPage via the client

--- src/Request/CheckoutPage/InitCheckoutPageRequest.php
+++ src/Request/CheckoutPage/InitCheckoutPageRequest.php
@@ -7,4 +7,9 @@ use Hochstrasser\Wirecard\Request\AbstractPaymentRequest;
 class InitCheckoutPageRequest extends AbstractPaymentRequest
 {
     protected $endpoint = 'https://checkout.wirecard.com/page/init.php';
+
+    function createHttpResponse()
+    {
+        throw new \Exception("Request can't be sent. Use a form instead to send this request");
+    }
 }
